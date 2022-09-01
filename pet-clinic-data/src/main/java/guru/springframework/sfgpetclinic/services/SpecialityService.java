49f6957d27a745e178c4df82ea71ab44131a16e4
zainhabib <zain.n.habib@gmail.com>
2022-08-31 20:44:13 -0500

    Create Specialty Service and impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
