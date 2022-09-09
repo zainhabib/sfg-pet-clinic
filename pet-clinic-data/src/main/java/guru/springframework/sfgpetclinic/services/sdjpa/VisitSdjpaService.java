5eed40989d7b7c729907b162dfcce7d58caba069
zainhabib <zain.n.habib@gmail.com>
2022-09-08 21:19:26 -0500

    Implement JPA Visit Service, add loader data. Closes #47.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/sdjpa/VisitSdjpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/sdjpa/VisitSdjpaService.java
@@ -1,2 +1,48 @@
-package guru.springframework.sfgpetclinic.services.sdjpa;public class VisitSdjpaService {
+package guru.springframework.sfgpetclinic.services.sdjpa;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.repositories.VisitRepository;
+import guru.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSdjpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSdjpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
 }
